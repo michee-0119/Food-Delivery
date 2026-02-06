93771f2c9af8608e2d6bc85aee4e7ccb8967d068
25LP2040 25LP2040 <michibaysaa@gmail.com>
2026-02-06 11:27:54 +0800

    f

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -14,10 +14,8 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", userRouter);
+// app.use("/users", userRouter);
 // app.use("/food", foodRouter);
-// app.use("/food-orders", foodOrderRouter);
-// app.use("/food-category", categoryRouter);
 
 connectToMongoDb();
 
